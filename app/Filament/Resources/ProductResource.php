780a3e282efd19894c87c960ed6b2413f31bdc5b
Luka Grabnar <grabnar.luka@gmail.com>
2024-12-29 22:09:42 +0100

    product images

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -10,6 +10,8 @@ use Filament\Facades\Filament;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -19,7 +21,9 @@ class ProductResource extends Resource
 {
   protected static ?string $model = Product::class;
 
-  protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+  protected static ?string $navigationIcon = 'heroicon-s-queue-list';
+
+  protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
 
   public static function form(Form $form): Form
   {
@@ -92,6 +96,11 @@ class ProductResource extends Resource
   {
     return $table
       ->columns([
+        Tables\Columns\SpatieMediaLibraryImageColumn::make('images')
+          ->collection('images')
+          ->limit(1)
+          ->label('Image')
+          ->conversion('thumb'),
         Tables\Columns\TextColumn::make('title')
           ->sortable()
           ->words(10)
@@ -132,7 +141,17 @@ class ProductResource extends Resource
       'index' => Pages\ListProducts::route('/'),
       'create' => Pages\CreateProduct::route('/create'),
       'edit' => Pages\EditProduct::route('/{record}/edit'),
+      'images' => Pages\ProductImages::route('/{record}/images'),
     ];
+  }
+
+  public static function getRecordSubNavigation(Page $page): array
+  {
+    return
+      $page->generateNavigationItems([
+        Pages\EditProduct::class,
+        Pages\ProductImages::class,
+      ]);
   }
 
   public static function canViewAny(): bool
