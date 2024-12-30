f22a4476d8ee02d8251b766be5392c8ebb2290f7
Luka Grabnar <grabnar.luka@gmail.com>
2024-12-30 17:43:56 +0100

    changed sidebar width

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
       ->default()
       ->id('admin')
       ->path('admin')
+      ->sidebarWidth('14')
       ->login()
       ->colors([
         'primary' => Color::Amber,
