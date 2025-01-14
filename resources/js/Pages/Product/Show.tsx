2af0527014a9b3f61f3016b8f453685ccedaea6b
lukag <grabnar.luka@gmail.com>
2025-01-14 15:34:48 +0100

    single product page start

---
+++ resources/js/Pages/Product/Show.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Product} from "@/types";
+
+function Show({product, variationOptions}: {product: Product, variationOptions: number[]}) {
+  return (
+    <div>Test</div>
+  );
+}
+
+export default Show;
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -7,12 +7,36 @@ export interface User {
     email_verified_at?: string;
 }
 
+export type Image = {
+  id: number;
+  thumb: string;
+  small: string;
+  large: string;
+}
+
+export type VariationTypeOption = {
+  id: number;
+  name: string;
+  images: Image[];
+  type: VariationType;
+}
+
+export type VariationType = {
+  id: number;
+  name: string;
+  type: 'select' | 'radio' | 'image';
+  options: VariationTypeOption[]
+}
+
 export type Product = {
   id: number;
   title: string;
   slug: string;
   price: number;
   image: string;
+  images: Image[];
+  short_description: string;
+  description: string;
   user: {
     id: number;
     name: string;
@@ -21,6 +45,13 @@ export type Product = {
     id: number;
     name: string;
   };
+  variationTypes: VariationType[],
+  variations: Array<{
+    id: number;
+    variation_type_options_id: number[],
+    quantity: number;
+    price: number;
+  }>
 }
 
 export type PaginationProps<T> = {
